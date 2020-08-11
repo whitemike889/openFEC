1f2ababd82c1986d74aa6e5ce8402f54abd91656
Priyanka Kasireddy <pkasireddy@fec.gov>
2020-08-11 10:42:23 -0400

    Test api builds with latest buildpack 1.7.17

--- tasks.py
+++ tasks.py
@@ -142,7 +142,7 @@ def _detect_space(repo, branch=None, yes=False):
 DEPLOY_RULES = (
     ('prod', _detect_prod),
     ('stage', lambda _, branch: branch.startswith('release')),
-    ('dev', lambda _, branch: branch == 'develop'),
+    ('dev', lambda _, branch: branch == 'feature/test-buildpkg-1.7.17'),
 )
 
 
