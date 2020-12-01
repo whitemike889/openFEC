173a8c47ad4cbf4dfd1670b73a72d4b6104b0a82
Priyanka Kasireddy <pkasireddy@fec.gov>
2020-12-01 16:09:08 -0500

    Deploy and test ES7 on dev space.

--- tasks.py
+++ tasks.py
@@ -143,7 +143,8 @@ def _detect_space(repo, branch=None, yes=False):
 DEPLOY_RULES = (
     ('prod', _detect_prod),
     ('stage', lambda _, branch: branch.startswith('release')),
-    ('dev', lambda _, branch: branch == 'develop'),
+    ('dev', lambda _, branch: branch == 'test-upgrade-es7-service'),
+    # ('dev', lambda _, branch: branch == 'develop'),
 )
 
 
