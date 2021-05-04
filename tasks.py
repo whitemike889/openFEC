4e98d1d8fb9997b7ac8cd2f5d59edeb40559d2dd
Jun Li <jli@fec.gov>
2021-05-04 12:26:18 -0400

    test/feature/4745-cache-cmte-cand-totals-endpoint

--- tasks.py
+++ tasks.py
@@ -142,7 +142,7 @@ def _detect_space(repo, branch=None, yes=False):
 
 DEPLOY_RULES = (
     ('prod', _detect_prod),
-    ('stage', lambda _, branch: branch.startswith('release')),
+    ('stage', lambda _, branch: branch == 'test/feature/4745-cache-cmte-cand-totals-endpoint'),
     ('dev', lambda _, branch: branch == 'develop'),
 )
 
